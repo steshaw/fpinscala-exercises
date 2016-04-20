40226f993acbdf4acb2dab0e2526f5c831fa45a0
Steven Shaw <steven@steshaw.org>
2016-04-20 19:27:16 +1000

    Most of the List exercises.

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -23,7 +23,7 @@ object List { // `List` companion object. Contains functions for creating and wo
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 
-  val x = List(1,2,3,4,5) match {
+  val x_ = List(1,2,3,4,5) match {
     case Cons(x, Cons(2, Cons(4, _))) => x
     case Nil => 42
     case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
@@ -50,19 +50,76 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = sys.error("todo")
+  def tail[A](l: List[A]): List[A] = l match {
+    case Cons(x, xs) => xs
+  }
 
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+  def setHead[A](l: List[A], h: A): List[A] = Cons(h, tail(l))
 
-  def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
+  @annotation.tailrec
+  def drop[A](l: List[A], n: Int): List[A] = if (n <= 0) l else drop(tail(l), n - 1)
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = sys.error("todo")
+  @annotation.tailrec
+  def dropWhile[A](l: List[A])(f: A => Boolean): List[A] = l match {
+    case Nil => l
+    case Cons(x, xs) => if (f(x)) dropWhile(xs)(f) else l
+  }
 
-  def init[A](l: List[A]): List[A] = sys.error("todo")
+  def init[A](l: List[A]): List[A] = l match {
+    case Cons(_, Nil) => Nil
+    case Cons(x, xs) => Cons(x, init(xs))
+  }
 
-  def length[A](l: List[A]): Int = sys.error("todo")
+  def length[A](l: List[A]): Int = foldRight(l, 0)((_, len) => len + 1)
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
+  @annotation.tailrec
+  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Nil => z
+    case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+  }
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
+  def sum3(ns: List[Int]) = foldLeft(ns, 0)(_ + _)
+
+  def product3(ns: List[Double]) = foldLeft(ns, 1.0)(_ * _)
+
+  def reverse[A](as: List[A]) = foldLeft(as, Nil:List[A])((a, b) => Cons(b, a))
+
+  // TODO: Ex 3.13
+
+  def append2[A](a1: List[A], a2: List[A]): List[A] = foldRight(a1, a2)(Cons(_, _))
+
+  // TODO: Ex 3.15
+
+  def add1(ns: List[Int]): List[Int] = foldRight(ns, Nil:List[Int])((n, acc) => Cons(n+1, acc))
+
+  def doubleToString(ds: List[Double]): List[String] = foldRight(ds, Nil:List[String])((n, acc) => Cons(n.toString, acc))
+
+  def map[A,B](l: List[A])(f: A => B): List[B] = foldRight(l, Nil:List[B])((a, b) => Cons(f(a), b))
+
+  def filter[A](as: List[A])(f: A => Boolean): List[A] =
+    foldRight(as, Nil:List[A])((a, b) => if (f(a)) Cons(a, b) else b)
+
+  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] = foldRight(as, Nil:List[B])((a, b) =>
+    append2(f(a), b) // TODO: append?
+  )
+
+  def filter2[A](as: List[A])(f: A => Boolean): List[A] =
+    flatMap(as)(a => if (f(a)) List(a) else List())
+
+  def addEm(as: List[Int], bs: List[Int]): List[Int] = (as, bs) match {
+    case (Cons(a, as_), Cons(b, bs_)) => Cons(a + b, addEm(as_, bs_))
+    case _ => Nil
+  }
+
+  def zipWith[A, B, C](as: List[A], bs: List[B])(f: (A, B) => C): List[C] = {
+    (as, bs) match {
+      case (Nil, Nil) => Nil
+      case (Cons(a, as_), Cons(b, bs_)) => Cons(f(a, b), zipWith(as_, bs_)(f))
+      case _ => Nil
+    }
+  }
+
+  // TODO: Ex 3.24
+  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = ???
+
 }
