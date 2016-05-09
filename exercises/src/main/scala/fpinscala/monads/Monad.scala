b7e29036ce85869b3e4dd1f269b4da3aa8d5655d
Steven Shaw <steven@steshaw.org>
2016-05-09 19:51:19 +1000

    Implement `Monad[Id]`.

--- exercises/src/main/scala/fpinscala/monads/Monad.scala
+++ exercises/src/main/scala/fpinscala/monads/Monad.scala
@@ -132,16 +132,16 @@ object Monad {
   }
 
   val idMonad: Monad[Id] = new Monad[Id] {
-    override def unit[A](a: => A): Id[A] = ???
-    override def flatMap[A, B](ma: Id[A])(f: (A) => Id[B]): Id[B] = ???
+    override def unit[A](a: => A): Id[A] = Id(a)
+    override def flatMap[A, B](ma: Id[A])(f: (A) => Id[B]): Id[B] = ma.flatMap(f)
   }
 
   def readerMonad[R] = ???
 }
 
 case class Id[A](value: A) {
-  def map[B](f: A => B): Id[B] = ???
-  def flatMap[B](f: A => Id[B]): Id[B] = ???
+  def map[B](f: A => B): Id[B] = Id(f(value))
+  def flatMap[B](f: A => Id[B]): Id[B] = f(value)
 }
 
 object Reader {
