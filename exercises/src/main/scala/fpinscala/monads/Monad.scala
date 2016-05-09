4a13382a6e6aa8f63fe62aa464ffe432be14cd37
Steven Shaw <steven@steshaw.org>
2016-05-09 19:18:14 +1000

    Implement `join` and flatMap in terms of join and map.

--- exercises/src/main/scala/fpinscala/monads/Monad.scala
+++ exercises/src/main/scala/fpinscala/monads/Monad.scala
@@ -70,14 +70,14 @@ trait Monad[M[_]] extends Functor[M] {
     a => flatMap(f(a))(g)
 
   // Implement in terms of `compose`:
-  def _flatMap[A,B](ma: M[A])(f: A => M[B]): M[B] = {
+  def _flatMap[A,B](ma: M[A])(f: A => M[B]): M[B] =
     compose(Function.const(ma), f)(())
-  }
 
-  def join[A](mma: M[M[A]]): M[A] = ???
+  def join[A](mma: M[M[A]]): M[A] = flatMap(mma)(identity)
 
   // Implement in terms of `join`:
-  def __flatMap[A,B](ma: M[A])(f: A => M[B]): M[B] = ???
+  def __flatMap[A,B](ma: M[A])(f: A => M[B]): M[B] =
+    join(map(ma)(f))
 }
 
 case class Reader[R, A](run: R => A)
