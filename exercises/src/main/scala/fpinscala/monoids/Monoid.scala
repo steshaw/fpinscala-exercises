599698cb8a33c82d8ca3c1d9d8daf54ec384fb99
Steven Shaw <steven@steshaw.org>
2016-05-09 14:11:34 +1000

    Implement `productMonoid`.

--- exercises/src/main/scala/fpinscala/monoids/Monoid.scala
+++ exercises/src/main/scala/fpinscala/monoids/Monoid.scala
@@ -144,8 +144,12 @@ object Monoid {
     }
   }
 
-  def productMonoid[A,B](A: Monoid[A], B: Monoid[B]): Monoid[(A, B)] =
-    sys.error("todo")
+  def productMonoid[A,B](ma: Monoid[A], mb: Monoid[B]): Monoid[(A, B)] = new Monoid[(A, B)] {
+    override def op(ab1: (A, B), ab2: (A, B)): (A, B) =
+      (ma.op(ab1._1, ab2._1), mb.op(ab1._2, ab2._2))
+
+    override def zero: (A, B) = (ma.zero, mb.zero)
+  }
 
   def functionMonoid[A,B](B: Monoid[B]): Monoid[A => B] =
     sys.error("todo")
