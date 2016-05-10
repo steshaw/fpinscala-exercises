a7a39ac315dafa706300a5a0d47eea499b3445ee
Steven Shaw <steven@steshaw.org>
2016-05-10 11:01:35 +1000

    Implement map3 and map4.

--- exercises/src/main/scala/fpinscala/applicative/Applicative.scala
+++ exercises/src/main/scala/fpinscala/applicative/Applicative.scala
@@ -19,6 +19,17 @@ trait Applicative[F[_]] extends Functor[F] {
   def map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
     apply(map(fa)(f.curried))(fb)
 
+  def map3[A,B,C,D](
+    fa: F[A], fb: F[B], fc: F[C]) (f: (A, B, C) => D
+  ): F[D] =
+    apply(apply(map(fa)(f.curried))(fb))(fc)
+
+  def map4[A,B,C,D,E](
+    fa: F[A], fb: F[B], fc: F[C], fd: F[D])(f: (A, B, C, D) => E
+  ): F[E] =
+    apply(apply(apply(map(fa)(f.curried))(fb))(fc))(fd)
+
+
   def sequence[A](fas: List[F[A]]): F[List[A]] = traverse(fas)(identity)
 
   def traverse[A,B](as: List[A])(f: A => F[B]): F[List[B]] =
