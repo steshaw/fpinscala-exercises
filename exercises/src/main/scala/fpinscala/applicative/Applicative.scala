e491af9936ae232212849ed205f9d23b3a204174
Steven Shaw <steven@steshaw.org>
2016-05-10 10:53:20 +1000

    Implement map/map2 in terms of apply/unit and vice versa.

--- exercises/src/main/scala/fpinscala/applicative/Applicative.scala
+++ exercises/src/main/scala/fpinscala/applicative/Applicative.scala
@@ -10,15 +10,14 @@ import language.higherKinds
 import language.implicitConversions
 
 trait Applicative[F[_]] extends Functor[F] {
-
-  def map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] = ???
-
-  def apply[A,B](fab: F[A => B])(fa: F[A]): F[B] = ???
-
   def unit[A](a: => A): F[A]
 
-  def map[A,B](fa: F[A])(f: A => B): F[B] =
-    apply(unit(f))(fa)
+  def apply[A,B](fab: F[A => B])(fa: F[A]): F[B] =
+    map2(fab, fa)((f, a) => f(a))
+
+  def map[A,B](fa: F[A])(f: A => B): F[B] = apply(unit(f))(fa)
+  def map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+    apply(map(fa)(f.curried))(fb)
 
   def sequence[A](fas: List[F[A]]): F[List[A]] = traverse(fas)(identity)
 
