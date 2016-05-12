1271ce92da8365aa71d52d7babcab99cc8eed5af
Steven Shaw <steven@steshaw.org>
2016-05-12 12:36:20 +1000

    Implement `fill`.

--- exercises/src/main/scala/fpinscala/localeffects/LocalEffects.scala
+++ exercises/src/main/scala/fpinscala/localeffects/LocalEffects.scala
@@ -98,7 +98,12 @@ sealed abstract class STArray[S,A](implicit manifest: Manifest[A]) {
   // Turn the array into an immutable list
   def freeze: ST[S,List[A]] = ST(value.toList)
 
-  def fill(xs: Map[Int,A]): ST[S,Unit] = ???
+  def fill(xs: Map[Int,A]): ST[S,Unit] = new ST[S, Unit] {
+    override protected def run(s: S): (Unit, S) = {
+      xs.foreach{case (i, a) ⇒ write(i, a)}
+      ((), s)
+    }
+  }
 
   def swap(i: Int, j: Int): ST[S,Unit] = for {
     x <- read(i)
