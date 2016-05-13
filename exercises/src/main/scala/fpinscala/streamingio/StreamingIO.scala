cf322df3f7f5c7e6bc7dc24c0ffc095dfaa89722
Steven Shaw <steven@steshaw.org>
2016-05-13 23:33:50 +1000

    Implement `runLog` using `MonadCatch`.

--- exercises/src/main/scala/fpinscala/streamingio/StreamingIO.scala
+++ exercises/src/main/scala/fpinscala/streamingio/StreamingIO.scala
@@ -626,7 +626,18 @@ object GeneralizedStreamTransducers {
      * below, this is not tail recursive and responsibility for stack safety
      * is placed on the `Monad` instance.
      */
-    def runLog(implicit F: MonadCatch[F]): F[IndexedSeq[O]] = ???
+    def runLog(implicit F: MonadCatch[F]): F[IndexedSeq[O]] = {
+      def go(p: Process[F, O], log: IndexedSeq[O]): F[IndexedSeq[O]] =
+        p match {
+          case Emit(o, tp) ⇒ go(tp, log :+ o)
+          case Halt(End) ⇒ F.unit(log)
+          case Halt(err) ⇒ F.fail(err)
+          case Await(req, recv) ⇒ F.flatMap(F.attempt(req)) { e ⇒
+            go(Try(recv(e)), log)
+          }
+        }
+      go(this, IndexedSeq())
+    }
 
     /*
      * We define `Process1` as a type alias - see the companion object
