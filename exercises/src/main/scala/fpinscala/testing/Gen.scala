52d462f4c60ed45929699f556e58f099c7387dcf
Steven Shaw <steven@steshaw.org>
2016-05-16 19:19:48 +1000

    Implement `Gen.choose`.

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -8,11 +8,6 @@ import Gen._
 import Prop._
 import java.util.concurrent.{Executors,ExecutorService}
 
-/*
-The library developed in this chapter goes through several iterations. This file is just the
-shell, which you can fill in and modify while working through the chapter.
-*/
-
 trait Prop { p1 ⇒
   def check: Boolean
 
@@ -25,16 +20,30 @@ object Prop {
   def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
 }
 
-object Gen {
-  def unit[A](a: => A): Gen[A] = ???
+case class Gen[A](sample: State[RNG, A]) {
+  def map[B](f: A => B): Gen[B] = Gen(sample.map(f))
+  def flatMap[B](f: A => Gen[B]): Gen[B] = ???
 }
 
-trait Gen[A] {
-  def map[A,B](f: A => B): Gen[B] = ???
-  def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
+object Gen {
+  def unit[A](a: => A): Gen[A] = Gen(State.unit(a) )
+
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
+    sequence(List.fill(n)(g))
+
+  def sequence[S, A](fs: List[Gen[A]]): Gen[List[A]] =
+    Gen(State.sequence(fs.map(_.sample)))
+
+  def choose(start: Int, stopExclusive: Int): Gen[Int] = {
+    val state: State[RNG, Int] = State(RNG.nonNegativeLessThan(stopExclusive + start))
+    val gen: Gen[Int] = Gen(state)
+    gen.map(_ + start)
+  }
+
+  val testChoose = listOfN(10000, Gen.choose(0, 3)).
+    sample.run(RNG.Simple(0))._1.distinct.sorted == List(0, 1, 2)
 }
 
 trait SGen[+A] {
-
 }
 
